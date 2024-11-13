7237804bb424d48634d9472a1be464d4e44e861c
Shuaib Mursaleen <saulie077@gmail.com>
2024-11-13 08:36:06 +0530

    init repo

---
+++ backend/src/main/java/com/shuaib/oopcw/OopcwApplication.java
@@ -0,0 +1,13 @@
+package com.shuaib.oopcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopcwApplication.class, args);
+	}
+
+}
